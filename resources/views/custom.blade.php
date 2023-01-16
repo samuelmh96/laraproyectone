b6ac29783e9c7279e0b227d737d96d47a9102c32
Samu <samu.96.h@gmail.com>
2023-01-15 23:26:17 -0400

    rutas con nombres

---
+++ resources/views/custom.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Custom</title>
+</head>
+<body>
+    <h1>Samuel Medrano Heredia</h1>
+    <p>{{ $sms }}</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('custom/', function () {
+    $message = 'Esto es un disfraz';
+    return view('custom', ['sms' => $message]);
+});
+
+Route::get('/contacts', function () {
+    return "Contactanos";
+})->name('contactos');
